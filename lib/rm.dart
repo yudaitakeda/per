9cf4d218a34bfac31e5837a09fb21b49c28889b5
yudaitakeda <yudai.1009.takeda@icloud.com>
2024-06-10 14:51:27 +0900

    feat:rm.dartの内容記述

--- lib/rm.dart
+++ lib/rm.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
 
+// StatefulWidgetを継承するクラスを定義
+class Rmstate extends StatefulWidget {
+  const Rmstate({Key? key}) : super(key: key);
+
+  // createStateメソッドを実装して、Stateオブジェクトを返す
+  @override
+  _RmState createState() => _RmState();
+}
+
+// Stateクラスを定義
+class _RmState extends State<Rmstate> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Page'),
+      ),
+      body: const Center(
+        child: Text('This is the second page'),
+      ),
+    );
+  }
+}
